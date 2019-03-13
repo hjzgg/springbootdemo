9a93bd521285890af932a5a2e72c7d379c2b501f
hujunzheng <hujunzheng@cmos.chinamobile.com>
2019-03-13 16:30:32 +0800

    fixup! 动态代理 Autowired测试

--- src/main/java/com/hjzgg/example/springboot/async/MyAsyncTask.java
+++ src/main/java/com/hjzgg/example/springboot/async/MyAsyncTask.java
@@ -3,12 +3,17 @@ package com.hjzgg.example.springboot.async;
 import com.hjzgg.example.springboot.config.MyAsyncConfigurer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.TypeConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.DependencyDescriptor;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 /**
  * @author hujunzheng
@@ -19,6 +24,11 @@ public class MyAsyncTask {
 
     private static Logger LOGGER = LoggerFactory.getLogger(MyAsyncConfigurer.class);
 
+    /**
+     * @see DefaultListableBeanFactory#resolveDependency(DependencyDescriptor, String, Set, TypeConverter)
+     * <p>
+     * Spring Bean创建-解决依赖 参考链接：https://blog.csdn.net/finalcola/article/details/81537380
+     */
     @Lazy
     @Autowired
     private MyInnerAsyncTask myInnerAsyncTask;
