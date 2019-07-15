253079c3d025a7f32cfcf87c88d91d6641a52848
hujunzheng <hujunzheng@cmos.chinamobile.com>
2019-07-15 21:55:21 +0800

    注释配置中心相关内容

--- src/main/java/com/hjzgg/example/springboot/configuration/MyConfiguration1.java
+++ src/main/java/com/hjzgg/example/springboot/configuration/MyConfiguration1.java
@@ -3,7 +3,7 @@ package com.hjzgg.example.springboot.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class MyConfiguration1 extends MyConfiguration2{
 
     @Bean
