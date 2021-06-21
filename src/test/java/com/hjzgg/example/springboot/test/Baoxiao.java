b7aa4af566362237e453be2fd04b768d71a968b4
hujunzheng <hujunzheng@cmos.chinamobile.com>
2021-06-21 10:08:38 +0800

    fix baoxiao

--- src/test/java/com/hjzgg/example/springboot/test/Baoxiao.java
+++ src/test/java/com/hjzgg/example/springboot/test/Baoxiao.java
@@ -1,10 +1,9 @@
 package com.hjzgg.example.springboot.test;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -14,9 +13,9 @@ import java.util.Random;
 public class Baoxiao {
     public static void main(String[] args) {
         //cal(2400, 13, 1, 2, 3);
-        cal(1200, 8, 10);
-        cal(1200, 8, 11);
-        cal(1200, 8, 12);
+        cal(1200, 14, 4);
+        cal(1200, 14, 5);
+        cal(1200, 14, 6);
     }
 
     public static void cal(int amount, int days, int... months) {
@@ -29,13 +28,13 @@ public class Baoxiao {
             money[random.nextInt(money.length)] += 1;
         }
         for (int i = 0; i < money.length; ++i) {
-            int x = 1 + random.nextInt(8);
+            int x = 1 + random.nextInt(5);
             int i1 = random.nextInt(money.length);
             int i2 = random.nextInt(money.length);
-            if (money[i1] + x < 200) {
+            if (money[i1] + x < 150) {
                 money[i1] += x;
                 money[i2] -= x;
-            } else if (money[i2] + x < 200) {
+            } else if (money[i2] + x < 150) {
                 money[i1] -= x;
                 money[i2] += x;
             }
@@ -52,17 +51,19 @@ public class Baoxiao {
         int year = LocalDate.now().getYear();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         for (int k = 0, i = 0; i < md.length; ++i) {
-            LocalDate date = LocalDate.of(year, months[i], 1);
-            for (int j = 0; j < md[i]; ++j, ++k) {
-                while (true) {
-                    date = date.plusDays(1 + random.nextInt(2));
-                    DayOfWeek dayOfWeek = date.getDayOfWeek();
-                    if (!(dayOfWeek == DayOfWeek.SUNDAY || dayOfWeek == DayOfWeek.SATURDAY)) {
-                        break;
-                    }
+            List<LocalDate> workDays = new ArrayList<>();
+            for (LocalDate beginDate = LocalDate.of(year, months[i], 1), endDate = YearMonth.of(year, months[i]).atEndOfMonth(); endDate.compareTo(beginDate) >= 0; beginDate = beginDate.plusDays(1)) {
+                if (beginDate.getDayOfWeek() == DayOfWeek.SUNDAY || beginDate.getDayOfWeek() == DayOfWeek.SATURDAY) {
+                    continue;
                 }
+                workDays.add(beginDate);
+            }
+            while (workDays.size() > md[i]) {
+                workDays.remove(random.nextInt(workDays.size()));
+            }
+            for (int j = 0; j < md[i]; ++j, ++k) {
                 LocalTime time = LocalTime.of(random.nextInt(24), random.nextInt(60), random.nextInt(60));
-                LocalDateTime dateTime = LocalDateTime.of(date, time);
+                LocalDateTime dateTime = LocalDateTime.of(workDays.get(j), time);
                 System.out.println(dateTime.format(formatter) + "   " + money[k]);
             }
         }
